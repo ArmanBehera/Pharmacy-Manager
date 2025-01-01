3e8fe06fe9fe6a737a1f3260e3f4fb99a17818a3
Arman Behera <73575356+ArmanBehera@users.noreply.github.com>
2025-01-01 23:03:05 +0530

    User id successfully retrieved from the backend.

--- pharmacy_django/api/serializers.py
+++ pharmacy_django/api/serializers.py
@@ -1,21 +1,35 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.tokens import Token
 from rest_framework_simplejwt.exceptions import AuthenticationFailed
-from rest_framework import serializers
+from doctor.models import DoctorUser
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    '''
-        Customizes JWT's default serializer to include more information about the user
-    '''
+    """
+    Customizes JWT's default serializer to include more information about the user.
+    """
 
     @classmethod
     def get_token(cls, user) -> Token:
         token = super().get_token(user)
-
         token['role'] = user.role
         token['is_superuser'] = user.is_superuser
-
-        if user.is_verified == False:
-            return AuthenticationFailed('User is not verified. Contact Admin.')
-
         return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # Check if the user is verified
+        if not self.user.is_verified:
+            raise AuthenticationFailed('User is not verified. Contact Admin.')
+
+        # Add user_id based on role
+        if self.user.role != 'Doctor':
+            data['user_id'] = self.user.id
+        else:
+            try:
+                doctor_user = DoctorUser.objects.get(user=self.user)
+                data['user_id'] = doctor_user.id
+            except DoctorUser.DoesNotExist:
+                raise AuthenticationFailed('Associated doctor record not found.')
+
+        return data
